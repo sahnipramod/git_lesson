1f32fe566517d47f261a1cc5e0575574e32f77db
Pramod Sahani <sahni.pramod@gmail.com>
2018-01-27 17:18:40 +0530

    commit x --> y and changed 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(y):
         """takes a value and return the square value.
         use the ** operator
         """
-        return(x ** 2)
+        return(y ** 2)
 
-print(my_square(4))
+print(my_square(42))
