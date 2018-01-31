c1a01eaba5700a9c28129dff8c7f63576d8ba661
Pramod Sahani <sahni.pramod@gmail.com>
2018-01-31 15:21:45 +0530

    added asecond function for calculating square

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,13 @@
+
 def my_square(y):
         """takes a value and return the square value.
+
         use the * operator
         """
         return(y ** 2)
 
+def my_square2(x)
+    return(x * X)
+
+print(my_square(42))
 print(my_square(42))
